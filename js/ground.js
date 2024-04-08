89c0889e67df97a44a3049e0a45b59a1980f024e
Rush-King <114742850+Rush-King@users.noreply.github.com>
2024-04-08 16:04:50 +0400

    Add files via upload

---
+++ js/ground.js
@@ -0,0 +1,33 @@
+class Ground {
+
+  constructor(x,y,w,h) {
+
+    let options = {
+
+     isStatic:true
+
+    };
+    
+    this.body = Bodies.rectangle(x,y,w,h,options);
+    this.w=w;
+    this.h=h;
+    World.add(world,this.body);
+
+  }
+
+  show() {
+
+    let pos=this.body.position;
+
+    push();
+
+    rectMode(CENTER);
+    noStroke();
+    fill(255,255,255);
+    rect(pos.x,pos.y,this.w,this.h);
+
+    pop();
+
+  }
+
+}
